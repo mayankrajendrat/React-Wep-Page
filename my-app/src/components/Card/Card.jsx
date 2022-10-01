cbe03fa4b8b3fba46608b1d897d2320a59dd3f59
Mayank Tiwari <mayank_tiwari@epam.com>
2022-10-01 15:12:57 +0530

    Added CSS for Card Heading

--- my-app/src/components/Card/Card.jsx
+++ my-app/src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import './Card.css'
 function CardComponent() {
   return (
     <Card className="text-center">
